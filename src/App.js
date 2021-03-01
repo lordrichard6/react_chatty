3210288061ad59c3b80404bb667d7453e894375b
lordrichard6 <paulolopesreizinho@gmail.com>
2021-03-01 21:11:15 +0100

    create message components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { ChatEngine } from 'react-chat-engine'
+import { ChatEngine } from "react-chat-engine";
 
-import './App.css'
+import ChatFeed from "./components/ChatFeed";
+import "./App.css";
 
 const App = () => {
-    return (
-        <ChatEngine 
-            height='100vh'
-            projectID='42b614e7-cb20-43f9-96af-eceeec55a70d'
-            userName='l0rdR1chard'
-            userSecret='123123'
-        />
-    )
-}
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="42b614e7-cb20-43f9-96af-eceeec55a70d"
+      userName="l0rdR1chard"
+      userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/ChatFeed.js
@@ -0,0 +1,31 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
+const ChatFeed = (props) => {
+  const { chats, activeChat, userName, messages } = props;
+
+  const chat = chats && chats[activeChat];
+
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMessage = userName === message.sender.username;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: '100%' }} >
+
+        </div>
+      )
+    });
+  };
+
+  renderMessages();
+
+  return <div>ChatFeed</div>;
+};
+
+export default ChatFeed;
