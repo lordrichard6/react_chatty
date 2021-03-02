ad29ceca4524ea0b1dbeff6e9868736103b5d1f7
lordrichard6 <paulolopesreizinho@gmail.com>
2021-03-02 21:27:54 +0100

    rendermessages

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -29,8 +29,22 @@ const ChatFeed = (props) => {
   };
 
   renderMessages();
-
-  return <div>ChatFeed</div>;
+  if(!chat) return 'Loading...'
+  return (
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat.title}</div>
+        <div className="chat-subtitle">
+          {chat.people.map((person) => ` ${person.person.username}`)}
+        </div>
+      </div>
+      {renderMessages()}
+      <div style={{ height: '100px' }} />
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
+    </div>
+  )
 };
 
 export default ChatFeed;
