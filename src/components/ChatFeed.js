9a5d7f4d1a8bb5d3b21c475fb186823ada10713f
lordrichard6 <paulolopesreizinho@gmail.com>
2021-03-02 20:56:23 +0100

    conditional in chatfeed sender and reciever

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -13,13 +13,18 @@ const ChatFeed = (props) => {
     return keys.map((key, index) => {
       const message = messages[key];
       const lastMessageKey = index === 0 ? null : keys[index - 1];
-      const isMessage = userName === message.sender.username;
+      const isMyMessage = userName === message.sender.username;
 
       return (
-        <div key={`msg_${index}`} style={{ width: '100%' }} >
-
+        <div key={`msg_${index}`} style={{ width: "100%" }}>
+          <div className="message-block">
+            {isMyMessage ? <MyMessage /> : <TheirMessage />}
+          </div>
+          <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+            read-reciepts
+          </div>
         </div>
-      )
+      );
     });
   };
 
