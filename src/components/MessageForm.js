57e79c4c8651820359999149a5d785b578343477
lordrichard6 <paulolopesreizinho@gmail.com>
2021-03-05 10:46:15 +0100

    set messageForm

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,5 +1,59 @@
-const MessageForm = () => {
-  return <div>MessageForm</div>;
+import { useState } from "react";
+import { SendOutlined, PictureOutlined } from "@ant-design/icons";
+import { sendMessage, isTyping } from "react-chat-engine";
+
+const MessageForm = (props) => {
+  const [value, setvalue] = useState("");
+  const { chatId, creds } = props;
+
+  const handleChange = (e) => {
+    setvalue(e.target.value);
+
+    isTyping(props, chatId);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const text = value.trim();
+
+    if (text.length > 0) {
+      sendMessage(creds, chatId, { text });
+    }
+
+    setvalue("");
+  };
+
+  const handleUpload = (e) => {
+    sendMessage(creds, chatId, { files: e.target.files, text: "" });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="message-form">
+      <input
+        className="message-input"
+        placeholder="Send a message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <label htmlFor="upload-button">
+          <span className="image-button">
+              <PictureOutlined className="picture-icon" />
+          </span>
+      </label>
+      <input
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: "none" }}
+        onChange={handleUpload.bind(this)}
+      />
+      <button className="send-button" type='submit'>
+          <SendOutlined className="send-icon" />
+      </button>
+    </form>
+  );
 };
 
 export default MessageForm;
--- src/components/MyMessage.js
+++ src/components/MyMessage.js
@@ -1,17 +1,30 @@
 import { getMessages } from "react-chat-engine";
 
 const MyMessage = ({ message }) => {
-      if(message?.attachments?.length > 0) {
-          return (
-              <img src={getMessages.attachments[0].file} alt="message-attachments" className="message-image" style={{ float: 'right' }} />
-          )
-      }
+  if (message.attachments.length && message.attachments > 0) {
+    return (
+      <img
+        src={message.attachments[0].file}
+        alt="message-attachments"
+        className="message-image"
+        style={{ float: "right" }}
+      />
+    );
+  }
 
-      return (
-          <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#a07f54' }}>
-            {message.text}
-          </div>
-      )
+  return (
+    <div
+      className="message"
+      style={{
+        float: "right",
+        marginRight: "18px",
+        color: "white",
+        backgroundColor: "#a07f54",
+      }}
+    >
+      {message.text}
+    </div>
+  );
 };
 
 export default MyMessage;
